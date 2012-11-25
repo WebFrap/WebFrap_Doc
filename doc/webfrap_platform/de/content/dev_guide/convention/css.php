e4db22cb44b45bd305c7a57a5df2b87c456ac7ee
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-11-26 00:21:48 +0100

    code conventions erweitert

--- doc/webfrap_platform/de/content/dev_guide/convention/css.php
+++ doc/webfrap_platform/de/content/dev_guide/convention/css.php
@@ -1,1 +1,30 @@
-<h1>WebFrap Codekonvention</h1>
+<h1>Css</h1>
+
+<p>
+Die CSS Klassen werden jede einzeln untereinander aufgelistet. <br />
+</p>
+
+
+<?php start_highlight();  ?>
+
+/**
+* Comments Block
+*/
+
+tag
+{
+
+}
+
+.some_class
+{
+
+}
+
+#some_class
+{
+
+}
+
+
+<?php display_highlight( 'css' ); ?>
--- doc/webfrap_platform/de/content/dev_guide/convention/php.php
+++ doc/webfrap_platform/de/content/dev_guide/convention/php.php
@@ -6,49 +6,15 @@
 <?php echo PHP_TAG; ?>
 
 /*******************************************************************************
- ____      ____  ________  ______   ________  _______          _       _______
-|_  _|    |_  _||_   __  ||_   _ \ |_   __  ||_   __ \        / \     |_   __ \
-  \ \  /\  / /    | |_ \_|  | |_) |  | |_ \_|  | |__) |      / _ \      | |__) |
-   \ \/  \/ /     |  _| _   |  __'.  |  _|     |  __ /      / ___ \     |  ___/
-    \  /\  /     _| |__/ | _| |__) |_| |_     _| |  \ \_  _/ /   \ \_  _| |_
-     \/  \/     |________||_______/|_____|   |____| |___||____| |____||_____|
-
-                                       __.;:-+=;=_.
-                                    ._=~ -...    -~:
-                     .:;;;:.-=si_=s%+-..;===+||=;. -:
-                  ..;::::::..<mQmQW>  :::.::;==+||.:;        ..:-..
-               .:.:::::::::-_qWWQWe .=:::::::::::::::   ..:::-.  . -:_
-             .:...:.:::;:;.:jQWWWE;.+===;;;;:;::::.=ugwmp;..:=====.  -
-           .=-.-::::;=;=;-.wQWBWWE;:++==+========;.=WWWWk.:|||||ii>...
-         .vma. ::;:=====.<mWmWBWWE;:|+||++|+|||+|=:)WWBWE;=liiillIv; :
-       .=3mQQa,:=====+==wQWBWBWBWh>:+|||||||i||ii|;=$WWW#>=lvvvvIvv;.
-      .--+3QWWc:;=|+|+;=3QWBWBWWWmi:|iiiiiilllllll>-3WmW#>:IvlIvvv>` .
-     .=___<XQ2=<|++||||;-9WWBWWWWQc:|iilllvIvvvnvvsi|\'\?Y1=:{IIIIi+- .
-     ivIIiidWe;voi+|illi|.+9WWBWWWm>:<llvvvvnnnnnnn}~     - =++-
-     +lIliidB>:+vXvvivIvli_."$WWWmWm;:<Ilvvnnnnonnv> .          .- .
-      ~|i|IXG===inovillllil|=:"HW###h>:<lIvvnvnnvv>- .
-        -==|1i==|vni||i|i|||||;:+Y1""'i=|IIvvvv}+-  .
-           ----:=|l=+|+|+||+=:+|-      - --++--. .-
-                  .  -=||||ii:. .              - .
-                       -+ilI+ .;..
-                         ---.::....
-
-********************************************************************************
 *
-* @author      : Dominik Bonsch <dominik.bonsch@s-db.de>
+* @author      : Dominik Bonsch <dominik.bonsch@webfrap.net>
 * @date        :
-* @copyright   : Softwareentwicklung Dominik Bonsch <contact@webfrap.net>
-* @project     : 
-* @projectUrl  : 
+* @copyright   : Webfrap Developer Network <contact@webfrap.net>
+* @project     : Webfrap Web Frame Application
+* @projectUrl  : http://webfrap.net
 *
-* Dual licenced, if you want to develop free Software you can use WebFrap
-* with the conditions of GPLv3.
-* If GPLv3 is nothing for you contact us: <contact@webfrap.net>.
-* Depeding on what you want to do we have some commercial licences
-* from free of cost use to enterprise versions or conditions by negotiation.
-*
-* @licence     : GPLv3
-*
+* @licence     : BSD License see: LICENCE/BSD Licence.txt
+* 
 * @version: @package_version@  Revision: @package_revision@
 *
 * Changes:
@@ -56,7 +22,7 @@
 *******************************************************************************/
 <?php display_highlight( 'php' ); ?>
 
-<label></label>
+
 <?php start_highlight();  ?>
 ////////////////////////////////////////////////////////////////////////////////
 // Attributes
@@ -70,10 +36,19 @@
 
 /**
 * @package
+* @subpackage
 * @author
 */
 class Fubar
 {
+  
+  /**
+  * What, 
+  * Why, 
+  * Since Version if public and part of the interface
+  * @var string
+  */
+  public $publicValue = null;
 
 }
 
