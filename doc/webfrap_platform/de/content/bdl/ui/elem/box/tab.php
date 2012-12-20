0ab677d6a724531335b95cda0e810d0d93008084
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-20 11:37:16 +0100

    extended tab documentation

--- doc/webfrap_platform/de/content/bdl/ui/elem/box/tab.php
+++ doc/webfrap_platform/de/content/bdl/ui/elem/box/tab.php
@@ -1,10 +1,36 @@
+<h1>Box: Tabs</h1>
 
-<h1>Tab</h1>
+<p>Mit den Tabboxen können in Viewports.</p>
 
-<p>Hier könnte ihre Dokumentation stehen... Wenn sie endlich jemand schreiben würde...</p>
-
-<label>Hier wäre ein super Platz für ein Codebeispiel</label>
 <?php start_highlight(); ?>
-<_..._>
-</_..._>
+<box name="team_box" type="tab" size="full" >
+  <body>
+    
+    <tab name="assignments" type="sub" >
+      <body>
+        <reference name="human_resources" ></reference>
+      </body>
+    </tab>
+    
+    <tab name="team" type="sub" >
+      <body>
+       <item 
+         name="roles" 
+         type="role_list" 
+         source="project_activity"
+         class="project_roles" >
+         
+          <area name="project_activity" />
+         
+          <roles>
+            <role name="project_manager" ></role>
+            <role name="owner" ></role>
+            <role name="staff" ></role>
+          </roles>
+         
+        </item>
+      </body>
+    </tab>
+  </body>
+</box>
 <?php display_highlight( 'xml' ); ?>
