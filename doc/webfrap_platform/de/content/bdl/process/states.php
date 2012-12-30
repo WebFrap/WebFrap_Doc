8469ad2c9481dc19ef2edeacddef9c6d910edbf3
Dominik Bonsch <dominik.bonsch@webfrap.net>
2012-12-31 00:07:52 +0100

    extended doku for process states

--- doc/webfrap_platform/de/content/bdl/process/states.php
+++ doc/webfrap_platform/de/content/bdl/process/states.php
@@ -1,10 +1,70 @@
-
 <h1>States</h1>
 
-<p>Hier könnte ihre Dokumentation stehen... Wenn sie endlich jemand schreiben würde...</p>
+<p>States sind Status flags welche global oder auf Process Nodes abgelegt werden können.</p>
 
-<label>Hier wäre ein super Platz für ein Codebeispiel</label>
+<label>Globale states</label>
 <?php start_highlight(); ?>
-<_..._>
-</_..._>
+<processes>
+  
+  <!-- 
+    name: Name des Prozesses (Cname)
+    src: Die Datenquelle für den Process
+    module: Das Module zu welchem der Prozess gehört
+   -->
+  <process 
+    name="project_project-development"
+    src="project_project"
+    module="process" >
+    
+    <states>
+      <state name="fu_checked" type="boolean" >
+        <label>
+          <text lang="de" >Fu is checked</text>
+        </label>
+      </state>
+    <states>
+  
+  </process>
+
+</processes>
+<?php display_highlight( 'xml' ); ?>
+
+
+
+<label>Beispiel Process Node</label>
+<?php start_highlight(); ?>
+<processes>
+  
+  <!-- 
+    name: Name des Prozesses (Cname)
+    src: Die Datenquelle für den Process
+   -->
+  <process 
+    name="project_project-development"
+    src="project_project" >
+
+   
+    <!-- Nodes -->
+    <nodes>
+    
+      <!-- 
+        name: name des Prozessnodes (Cname)
+       -->
+      <node name="new"  >
+
+        <states>
+          <state name="fu_checked" type="boolean" >
+            <label>
+              <text lang="de" >Fu is checked</text>
+            </label>
+          </state>
+        <states>
+        
+      </node>
+     
+    </nodes>
+    
+  </process>
+
+</processes>
 <?php display_highlight( 'xml' ); ?>
