9c89b6a20cb1835053a77c1965bcbacdcde16a06
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-01-20 15:44:06 +0100

    improved the docu for the acls

--- doc/webfrap_platform/de/content/bdl/base_elements/access/path.php
+++ doc/webfrap_platform/de/content/bdl/base_elements/access/path.php
@@ -1,43 +1,48 @@
 <h1>Pfade</h1>
 
-<p>Hier könnte ihre Dokumentation stehen... Wenn sie endlich jemand schreiben würde...</p>
+<p>
+Mit den Pfaden können impliziete ACLs definiert werden.
+z.B der Manager einer OrgUnit hat update Rechte auf alle Projekte die seiner
+Orgunit zugewiesen werden.
 
-<label>Hier wäre ein super Platz für ein Codebeispiel</label>
+</p>
+
+<label>Beispiel</label>
 <?php start_highlight(); ?>
 <_..._>
   <access>
-  
+
     <!-- Pfade definieren -->
     <paths>
-      
+
       <!-- Checks die in allen Pfade ausgeführt werden sollen -->
       <checks>
         <check>
-        
+
         </check>
       </checks>
-      
+
       <!-- Set für alle Pfade, wenn all Checks erfolgreich waren -->
       <set>
-      
+
       </set>
-      
+
       <references>
         <ref name="project_task" >
-          
+
           <checks>
             <check ref_id="id_company" >
               <roles>
                 <role name="" />
               </roles>
-              
+
               <level value="" />
-              
+
               <!-- Set wenn dieser Check erfolgreich war -->
               <set>
-              
+
               </set>
-              
+
             </check>
           </checks>
 
@@ -45,18 +50,18 @@
           <set>
             <roles>
             </roles>
-            
+
             <level value="" ref="" />
           </set>
-        
+
           <!-- Refrerences sind rekursiv verschachtelbar  -->
           <references>
             <ref name="task_employee" >
-              
+
               <!-- Interne Checks -->
               <checks>
               </checks>
-            
+
               <references>
                 <ref name="task_employee_contract" >
                   <!-- Interne Checks -->
@@ -66,7 +71,7 @@
               </references>
             </ref>
           </references>
-          
+
         </ref>
       </references>
     </paths>
