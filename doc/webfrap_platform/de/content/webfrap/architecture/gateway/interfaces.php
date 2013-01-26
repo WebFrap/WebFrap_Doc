4424ca65dd15c0f41d5afc9a23faceb48d9bdb53
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-01-26 19:08:20 +0100

    removed or replaced remaining subwindow elements

--- doc/webfrap_platform/de/content/webfrap/architecture/gateway/interfaces.php
+++ doc/webfrap_platform/de/content/webfrap/architecture/gateway/interfaces.php
@@ -5,8 +5,8 @@
   <li><span class="wbf_gateway" >index.php</span></li>
   <li><span class="wbf_gateway" >ajax.php</span></li>
   <li><span class="wbf_gateway" >html.php</span></li>
-  <li><span class="wbf_gateway" >subwindow.php</span></li>
   <li><span class="wbf_gateway" >document.php</span></li>
   <li><span class="wbf_gateway" >file.php</span></li>
   <li><span class="wbf_gateway" >service.php</span></li>
+  <li><span class="wbf_gateway" >modal.php</span></li>
 </ul>
