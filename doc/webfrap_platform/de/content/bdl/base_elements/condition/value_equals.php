e95be3379d25cef5c7e1898ab7795465666efb37
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-15 15:18:14 +0100

    updated condition desc

--- doc/webfrap_platform/de/content/bdl/base_elements/condition/value_equals.php
+++ doc/webfrap_platform/de/content/bdl/base_elements/condition/value_equals.php
@@ -17,9 +17,24 @@ Eine einfache == condition.
     <attribute 
       name="id_category.access_key" 
       value="product"  />
-      
+    
+    <!-- optionaler / ondemand negator -->
     <not />
     
   </condition>
 </conditions>
 <?php display_highlight( 'xml' ); ?>
+
+
+## Example Code: SwitchCheck
+<?php start_highlight(); ?>
+
+!$row['project_category_access_key'] == 'product'
+
+<?php display_highlight( 'php' ); ?>
+
+Zugegeben, das sieht wenig aus, allerdings muss man bedenken, dass auch alle
+für die checks nötigen tabellen dazu gejoint werden, sowie die nötigen felder
+in die queries eingebunden werden. 
+
+So betrachtet kommt also noch einiges an Code und logik dazu die eingespart wird.
