9d06d22477289c10b4414dca646eaa934730d8b4
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-16 21:28:53 +0100

    started to rewrite the documentor system

---
+++ doc/documentor/login_form.php
@@ -0,0 +1,19 @@
+<div style="width:300px;margin:0px auto;" >
+  <form action="login.php" method="post" >
+    <fieldset>
+      <legend>Login</legend>
+      
+      <div style="margin:25px;" >
+        <div>
+          <label>User:</label><input type="text" name="user"  /><br />
+          <label>Password:</label><input type="password" name="passwd"  /><br />
+        </div>
+        <div style="margin-top:30px;" >
+          <input type="submit" name="Login"  />
+        </div>
+      </div>
+      
+    </fieldset>
+  </form>
+</div>
+
