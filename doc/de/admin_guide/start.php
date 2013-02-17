3d81c70e1109c8c0e449155c29b7e5555fa9628f
Dominik Bonsch <dominik.bonsch@webfrap.net>
2013-02-17 11:51:01 +0100

    moved docu to the webfrap repository

--- doc/de/admin_guide/start.php
+++ doc/de/admin_guide/start.php
@@ -1,14 +1,13 @@
-<h1>Admin guidance and process framework</h1>
+# Admin guidance and process framework
 
-<p>
+
 Selbst die beste Flinte ist nutzlos, wenn der Schütze nicht weiß wo die Kugel
 am Ende raus kommt.<br />
-Solange das System nicht vollkommen autark alle Aufgaben alleine bewältigt steht 
+Solange das System nicht vollkommen autark alle Aufgaben alleine bewältigt steht
 und fällt der Erfolg mit der Kompetenz der Betreiber und Anwender.
-</p>
 
-<p>
-Das "Admin guidance and process framework" für die <?php kw('platform.name') ?> enthält 
+
+Das "Admin guidance and process framework" für die <?php kw('platform.name') ?> enthält
 wichtigen Grundlagen für den Betrieb einer WebFrap basierten Business Applikation.
-</p>
+
 
